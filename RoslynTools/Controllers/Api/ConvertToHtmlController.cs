b9fcc2c070a890bae8b950b906ec4166bffc2936
Toni Petrina <tonipetrina@hotmail.com>
2014-11-06 10:24:40 -0800

    Basic C# to HTML converter

---
+++ RoslynTools/Controllers/Api/ConvertToHtmlController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.Text;
+
+namespace RoslynTools.Controllers.Api
+{
+    public class ConvertToHtmlController : ApiController
+    {
+        public async Task<object> Post([FromBody] string code)
+        {
+            code = code ?? string.Empty;
+
+            try
+            {
+                var workspace = new CustomWorkspace();
+                var solution = workspace.CurrentSolution;
+                var project = solution.AddProject("projectName", "assemblyName", LanguageNames.CSharp);
+                var document = project.AddDocument("name.cs", code);
+                var text = await document.GetTextAsync();
+
+                var classifiedSpans =
+                    (await Classifier.GetClassifiedSpansAsync(document, TextSpan.FromBounds(0, text.Length)))
+                        .ToList();
+
+                var sb = new StringBuilder();
+                var current = 0;
+                foreach (var classifiedSpan in classifiedSpans)
+                {
+                    var span = classifiedSpan.TextSpan;
+                    if (span.Start > current)
+                    {
+                        sb.Append(code.Substring(current, span.Start - current));
+                    }
+
+                    sb.AppendFormat("<span class='{1}'>{0}</span>", code.Substring(span.Start, span.Length),
+                        classifiedSpan.ClassificationType.Replace(' ', '-'));
+
+                    current = span.End;
+                }
+
+                return sb.ToString();
+            }
+            catch (Exception)
+            {
+                return "Exception";
+            }
+        }
+    }
+}
---
+++ RoslynTools/Controllers/ConvertToHtmlController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace RoslynTools.Controllers
+{
+    public class ConvertToHtmlController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
