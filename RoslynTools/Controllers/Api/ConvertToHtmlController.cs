f28031fd910cc52236fa983f37330e0b6261e90b
Toni Petrina <tonipetrina@hotmail.com>
2014-12-01 23:46:09 +0100

    Refactored logic out of the API controller

--- RoslynTools/Controllers/Api/ConvertToHtmlController.cs
+++ RoslynTools/Controllers/Api/ConvertToHtmlController.cs
@@ -1,80 +1,13 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web.Http;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Classification;
-using Microsoft.CodeAnalysis.Formatting;
-using Microsoft.CodeAnalysis.Text;
 
 namespace RoslynTools.Controllers.Api
 {
     public class ConvertToHtmlController : ApiController
     {
-        public async Task<object> Post([FromBody] string code)
+        public async Task<CodeAsHtmlModel> Post([FromBody] string code)
         {
-            code = code ?? string.Empty;
-
-            try
-            {
-                using (var workspace = new CustomWorkspace())
-                {
-                    var solution = workspace.CurrentSolution;
-                    var project = solution.AddProject("newproject", "newassembly", LanguageNames.CSharp);
-                    var document = project.AddDocument("newfile.cs", code);
-
-                    document = await Formatter.FormatAsync(document);
-                    var text = await document.GetTextAsync();
-
-                    var sourceText = await document.GetTextAsync();
-                    code = sourceText.ToString();
-
-                    var classifiedSpans =
-                        (await Classifier.GetClassifiedSpansAsync(document, TextSpan.FromBounds(0, text.Length)))
-                            .ToList();
-
-                    var sb = new StringBuilder();
-                    var current = 0;
-                    foreach (var classifiedSpan in classifiedSpans)
-                    {
-                        var span = classifiedSpan.TextSpan;
-                        if (span.Start > current)
-                        {
-                            sb.Append(code.Substring(current, span.Start - current)
-                                .Replace(" ", "&nbsp")
-                                .Replace("\n", "<br />"
-                                ));
-                        }
-
-                        sb.AppendFormat("<span class='{1}'>{0}</span>", code.Substring(span.Start, span.Length)
-                            .Replace(" ", "&nbsp")
-                            .Replace("\n", "<br />"),
-                            classifiedSpan.ClassificationType.Replace(' ', '-'));
-
-                        current = span.End;
-                    }
-
-                    return sb.ToString();
-                }
-            }
-            catch (ReflectionTypeLoadException ex)
-            {
-                var sb = new StringBuilder();
-                sb.AppendLine(ex.ToString());
-                foreach (var e in ex.LoaderExceptions)
-                {
-                    sb.AppendLine("------------");
-                    sb.AppendLine(e.ToString());
-                }
-                return sb.ToString();
-            }
-
-            catch (Exception ex)
-            {
-                return "Exception: " + ex;
-            }
+            return RoslynTools.Common.CSharpToHtml.ConvertAsync(code);
         }
     }
 }
